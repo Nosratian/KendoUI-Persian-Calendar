57c8eec589d680974690c96ca1054699b0deff97
Mahdi Nosratian <Mahdi.Nosratian@gmail.com>
2018-03-03 17:47:19 +0330

    Add files via upload

---
+++ fa-IR.js
@@ -0,0 +1,64 @@
+﻿/* Iran Culture*/
+(function (window, undefined) {
+    kendo.cultures["fa-IR"] = {
+        name: "fa-IR",
+        numberFormat: {
+            pattern: ["-n"],
+            decimals: 2,
+            ",": ",",
+            ".": ".",
+            groupSize: [3],
+            percent: {
+                pattern: ["-n %", "n %"],
+                decimals: 2,
+                ",": ",",
+                ".": ".",
+                groupSize: [3],
+                symbol: "%"
+            },
+            currency: {
+                pattern: ["-$n", "$n"],
+                decimals: 2,
+                ",": ",",
+                ".": ".",
+                groupSize: [3],
+                symbol: "£"
+            }
+        },
+        calendars: {
+            standard: {
+                days: {
+                    names: ["يکشنبه", "دوشنبه", "سه شنبه", "چهارشنبه", "پنج شنبه", "جمعه", "شنبه"],
+                    namesAbbr: ["يک", "دو", "سه", "چهار", "پنج", "جمعه", "شنبه"],
+                    namesShort: ["ي", "د", "س", "چ", "پ", "ج", "ش"]
+                },
+                months: {
+                    names: ["فروردين", "ارديبهشت", "خرداد", "تير", "مرداد", "شهريور", "مهر", "آبان", "آذر", "دي", "بهمن", "اسفند", ""],
+                    namesAbbr: ["فروردين", "ارديبهشت", "خرداد", "تير", "مرداد", "شهريور", "مهر", "آبان", "آذر", "دي", "بهمن", "اسفند", ""]
+                },
+                AM: ["ق.ض", "ق.ض", "ق.ض"],
+                PM: ["ب.ض", "ب.ض", "ب.ض"],
+                patterns: {
+                    d: "yyyy/MM/dd",
+                    D: "yyyy/MM/dd",
+                    F: "yyyy/MM/dd HH:mm:ss",
+                    g: "yyyy/MM/dd HH:mm",
+                    G: "yyyy/MM/dd HH:mm:ss",
+                    m: "dd MMMM",
+                    M: "dd MMMM",
+                    s: "yyyy'-'MM'-'dd'T'HH':'mm':'ss",
+                    t: "HH:mm",
+                    T: "HH:mm:ss",
+                    u: "yyyy'-'MM'-'dd HH':'mm':'ss'Z'",
+                    y: "MMMM yyyy",
+                    Y: "MMMM yyyy"
+                },
+                "/": "/",
+                ":": ":",
+                firstDay: 6
+            }
+        }
+    }
+})(this);
+
+kendo.culture("fa-IR");
